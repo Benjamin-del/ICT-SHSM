cbd11e5560ca736e767f3840b9ad605d7421083e
Benjamin-del <benjamin.maheral@gmail.com>
2023-11-22 15:53:29 +0000

    Move mdArr to config.json

--- server.js
+++ server.js
@@ -3,20 +3,29 @@
 //  Run this file with node  (node server.js)        //
 ///////////////////////////////////////////////////////
 
+// Import Modules
 const fs = require('fs');
 const { marked } = require('marked');
-
-const config = require('./config.json');
-const mdDir = './md';
 const process = require('process');
+const config = require('./config.json');
 
+// Check if in dev mode
+const act = process.argv[2] === "-a" || false
+if (act) {
+    console.log("GH Pages Mode - No .html extension")
+} else {
+    console.log("Local Mode - .html extension")
 
-const farr = fs.readdirSync(mdDir)
+}
+
+// Map MD directory
+const farr = fs.readdirSync(config.mdDir)
+const pgdt = []
 farr.forEach((file, index, arr) => {
     // Tell Terminal which file is being worked on
     console.log("Working on file: " + file + " | " + (index + 1) + " of " + arr.length)
     // Create Path to file
-    const pth = mdDir + "/" + file
+    const pth = config.mdDir + "/" + file
     // Read file
     const md = fs.readFileSync(pth, 'utf8');
     // Convert to HTML
@@ -35,22 +44,7 @@ farr.forEach((file, index, arr) => {
     // Write to file (in P directory)
     fs.writeFileSync("./p/" + file.replace(".md", ".html"), parse, "utf8")
     // Tell Terminal which file is finished
-    console.log("Finished: " + file + " | " + (index + 1) + " of " + arr.length)
-});
-
-// Write to pages.json
-
-// Map Array to Object
-const act = process.argv[2] === "-a" || false
-if (act) {
-    console.log("GH Pages Mode - No .html extension")
-} else {
-    console.log("Local Mode - .html extension")
-
-}
-const pgdt = farr.map((file) => {
-    //
-    return {
+    pgdt.push({
         "name": file.replace(".md", "").replace("_", " "),
         "url": "/p/" + (function () {
             if (act) {
@@ -59,8 +53,12 @@ const pgdt = farr.map((file) => {
                 return file.replace(".md", ".html")
             }
         })(),
-    }
-})
+    })
+    console.log("Finished: " + file + " | " + (index + 1) + " of " + arr.length)
+});
 
-
-fs.writeFileSync("pages.json", JSON.stringify({ info: config, pages: pgdt}, null, 4), "utf8")
+// Write pages.json
+console.log("Updating Pages Database")
+fs.writeFileSync("pages.json", JSON.stringify({ info: config, pages: pgdt }, null, 4), "utf8")
+console.log("Page Database Updated")
+console.log("Process Complete")
